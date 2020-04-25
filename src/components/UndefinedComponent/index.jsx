5cc555d2ce6f3db4589f200385489983b3e1fdea
Thiago Régis <tregismoreira@gmail.com>
2020-04-25 04:06:53 -0300

    Remove UndefinedComponent.

--- src/components/UndefinedComponent/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UndefinedComponent = () => (
-  <div className="alert alert-warning" role="alert">
-    Undefined component.
-  </div>
-);
-
-export default UndefinedComponent;
