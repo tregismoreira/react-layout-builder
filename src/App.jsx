553d322cc9063c213a98182c8d0fa7ae6933ac2f
Thiago Régis <tregismoreira@gmail.com>
2020-04-25 01:20:57 -0300

    Add normalize.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ContainerComponent, RowComponent, ColComponent } from './components';
 
+import './assets/scss/main.scss';
+
 function App() {
   return (
     <div className="App">
